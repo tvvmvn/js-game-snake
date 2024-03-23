dac9cc3778c33be8b041d3b9985208c653368411
tvvmvn <tvvmvn@gmail.com>
2024-03-23 16:03:32 +0900

    3/23

--- _main.js
+++ _main.js
@@ -26,7 +26,7 @@
     }
   }
 
-  /* constants */
+  /* enums */
 
   const Direction = {
     UP: 0,
@@ -53,56 +53,16 @@
 
   /* variables */
 
-  var canvas  = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  canvas.style.backgroundColor = "#222";
-
-  addEventListener("keydown", keyDownHandler);
-
-  var snake = new Snake(
-    Stage.OFFSET_X + (Stage.CELL * 4), 
-    Stage.OFFSET_Y,
-    0,
-    Stage.CELL,
-    20,
-    true,
-    [
-      [Stage.OFFSET_X + (Stage.CELL * 4), Stage.OFFSET_Y],
-      [Stage.OFFSET_X + (Stage.CELL * 3), Stage.OFFSET_Y],
-      [Stage.OFFSET_X + (Stage.CELL * 2), Stage.OFFSET_Y],
-      [Stage.OFFSET_X + (Stage.CELL * 1), Stage.OFFSET_Y],
-      [Stage.OFFSET_X, Stage.OFFSET_Y]
-    ],
-    Direction.RIGHT,
-    "#0bf"
-  );
-
-  var apple = new Apple(
-    Stage.OFFSET_X + 100, 
-    Stage.OFFSET_Y + 100,
-    10,
-    10,
-    false,
-    "#0b0"
-  )
-
-  var time = {
-    _s: 0,
-    s: 0,
-  };
-
-  var game = {
-    over: false,
-    end: false
-  }
-
-  var prevX = snake.x;
-  var prevY = snake.y;
+  var snake;
+  var apple;
+  var time;
+  var game;
+  var prevX;
+  var prevY;
   var prevDir;
-
   var checker = [];
+  var start = false;
 
   for (var r = 0; r < Stage.HEIGHT / Stage.CELL; r++) {
     checker[r] = [];
@@ -116,67 +76,92 @@
     }
   }
 
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  canvas.style.backgroundColor = "#222";
+  addEventListener("keydown", keyDownHandler);
+
   /* run the game */
 
-  setInterval(() => {
+  setInterval(run, 10); // 100hz
+
+  function run() {
     clearCanvas();
-    drawBackground();
-    drawSnake();
+    drawTitle();
+    drawStage();
+
+    // Start screen
+    if (!start) {
+      initialize();
+      drawMessage("Press any key to start game");
+      return;
+    }
+
+    if (game.over || game.end) {
+      if (game.over) {
+        drawMessage("GAME OVER");
+      }
+  
+      if (game.end) {
+        drawMessage("YOU WIN!");
+      }
+    } else {
+      setSnake();
+      setApple();
+      setTime();
+    }
+
     drawApple();
+    drawSnake();
     drawTime();
     drawScore();
-
-    if (game.over) {
-      drawMessage("GAME OVER");
-    }
-
-    if (game.end) {
-      drawMessage("YOU WIN!");
-    }
-
-    setTime();
-  }, 10); // 100hz
-
-  /* draw */
-
-  function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-  
-  function drawBackground() {
-    // title
-    ctx.font = "20px Monospace";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#fff";
-    ctx.fillText("S N A K E", Stage.OFFSET_X + (Stage.WIDTH / 2), 40);
-
-    // checker
-    ctx.fillStyle = "#333";
-
-    for (var r = 0; r < checker.length; r++) {
-      for (var c = 0; c < checker[r].length; c++) {
-        if (checker[r][c]) {
-          ctx.fillRect(
-            Stage.OFFSET_X + (Stage.CELL * c),
-            Stage.OFFSET_Y + (Stage.CELL * r),
-            Stage.CELL, Stage.CELL);
-        }
-      }
-    }
-
-    // border
-    ctx.beginPath();
-    ctx.strokeStyle = "#ddd";
-    ctx.rect(Stage.OFFSET_X, Stage.OFFSET_Y, Stage.WIDTH, Stage.HEIGHT);
-    ctx.stroke();
   }
 
-  function drawSnake() {
-    if (!game.over && !game.end) {
-      snake.m++;
+  function initialize() {
+    snake = new Snake(
+      Stage.OFFSET_X + (Stage.CELL * 2), Stage.OFFSET_Y,
+      0,
+      Stage.CELL,
+      20,
+      true,
+      [
+        [Stage.OFFSET_X + (Stage.CELL * 2), Stage.OFFSET_Y],
+        [Stage.OFFSET_X + (Stage.CELL * 1), Stage.OFFSET_Y],
+        [Stage.OFFSET_X, Stage.OFFSET_Y]
+      ],
+      Direction.RIGHT,
+      "#0bf"
+    );
+
+    apple = new Apple(
+      Stage.OFFSET_X + 100, Stage.OFFSET_Y + 100,
+      10,
+      20,
+      false,
+      "#0b0"
+    )
+
+    time = {
+      _s: 0,
+      s: 0,
+    };
+
+    game = {
+      over: false,
+      end: false
     }
 
-    if (prevDir != snake.dir) {
+    prevX = snake.x;
+    prevY = snake.y;
+  }
+
+  /* functions */
+
+  // Snake
+  function setSnake() {
+    snake.m++;
+
+    if (prevDir !== snake.dir) {
       snake.m += snake.movingPoint;
     }
     
@@ -195,7 +180,7 @@
       }
     }
 
-    if (prevX != snake.x || prevY != snake.y) {
+    if (prevX !== snake.x || prevY !== snake.y) {
       snake.m = 0;
       
       if (wallCrash()) {
@@ -220,7 +205,109 @@
     prevX = snake.x;
     prevY = snake.y;
     prevDir = snake.dir;
+  }
 
+  function selfCrash() {
+    var value = false;
+
+    for (var h = 3; h < snake.node.length; h++) {
+      if (snake.x == snake.node[h][0] && snake.y == snake.node[h][1]) {
+        value = true;
+      }
+    }
+
+    return value;
+  }
+
+  function wallCrash() {
+    var leftCrash = snake.x < Stage.OFFSET_X
+    var rightCrash = snake.x + Stage.CELL > Stage.OFFSET_X + Stage.WIDTH;
+    var topCrash = snake.y < Stage.OFFSET_Y;
+    var bottomCrash = snake.y + Stage.CELL > Stage.OFFSET_Y + Stage.HEIGHT;
+
+    if (leftCrash || rightCrash || topCrash || bottomCrash) {
+      return true
+    }
+
+    return false;
+  }
+  
+  // Apple
+  function setApple() {
+    if (apple.count) {
+      apple.eaten = (snake.x == apple.x) && (snake.y == apple.y)
+
+      if (apple.eaten) {
+        snake.node.push([snake.x, snake.y]);
+        apple.count--;
+        putApple();
+        snake.movingPoint--;
+      }
+    } else {
+      game.end = true;
+    }
+  }
+
+  function putApple() {
+    var x = Stage.OFFSET_X + (Stage.CELL * (Math.floor(Math.random() * 20)));
+    var y = Stage.OFFSET_Y + (Stage.CELL * (Math.floor(Math.random() * 15)));
+
+    var putAgain = false;
+
+    // detect apple on snake body
+    for (var i = 0; i < snake.node.length; i++) {
+      if (snake.node[i][0] == x && snake.node[i][1] == y) {
+        putAgain = true;
+      }
+    }
+
+    if (putAgain) {
+      putApple()
+    } else {
+      apple.x = x
+      apple.y = y
+    }
+  }
+
+  // Time
+  function setTime() {
+    time._s++
+
+    if (time._s > 100) {
+      time.s++;
+      time._s = 0;
+    }
+  }
+
+  /* draw */
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+  
+  function drawStage() {
+    // checker
+    ctx.fillStyle = "#333";
+
+    for (var r = 0; r < checker.length; r++) {
+      for (var c = 0; c < checker[r].length; c++) {
+        if (checker[r][c]) {
+          ctx.fillRect(
+            Stage.OFFSET_X + (Stage.CELL * c),
+            Stage.OFFSET_Y + (Stage.CELL * r),
+            Stage.CELL, Stage.CELL);
+        }
+      }
+    }
+
+    // border
+    ctx.beginPath();
+    ctx.strokeStyle = "#ddd";
+    ctx.rect(Stage.OFFSET_X, Stage.OFFSET_Y, Stage.WIDTH, Stage.HEIGHT);
+    ctx.stroke();
+  }
+
+  function drawSnake() {
     for (var i = 0; i < snake.node.length; i++) {
       ctx.fillStyle = snake.color;
       ctx.fillRect(snake.node[i][0], snake.node[i][1], snake.size, snake.size);
@@ -228,35 +315,23 @@
   }
 
   function drawApple() {
-    if (apple.count) {
-      apple.eaten = (snake.x == apple.x) && (snake.y == apple.y);
+    ctx.beginPath();
+    ctx.arc(
+      apple.x + apple.radius,
+      apple.y + apple.radius,
+      10,
+      0,
+      2 * Math.PI
+    );
+    ctx.fillStyle = apple.color;
+    ctx.fill();
+  }
 
-      if (apple.eaten) {
-        snake.node.push([snake.x, snake.y]);
-        apple.count--;
-        
-        var [x, y] = getAppleCrds();
-
-        apple.x = x;
-        apple.y = y;
-
-        snake.movingPoint -= 2;
-      }
-
-      ctx.beginPath();
-      ctx.arc(
-        apple.x + apple.radius,
-        apple.y + apple.radius,
-        10,
-        0,
-        2 * Math.PI
-      );
-      ctx.fillStyle = apple.color;
-      ctx.fill();
-
-    } else {
-      game.end = true;
-    }
+  function drawTitle() {
+    ctx.font = "20px Monospace";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#fff";
+    ctx.fillText("S N A K E", Stage.OFFSET_X + (Stage.WIDTH / 2), 40);
   }
 
   function drawScore() {
@@ -284,68 +359,18 @@
     );
   }
 
-  /* functions */
-
-  function selfCrash() {
-    for (var h = 3; h < snake.node.length; h++) {
-      if (snake.x == snake.node[h][0] && snake.y == snake.node[h][1]) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  function wallCrash() {
-    var leftCrash = snake.x < Stage.OFFSET_X
-    var rightCrash = snake.x + Stage.CELL > Stage.OFFSET_X + Stage.WIDTH;
-    var topCrash = snake.y < Stage.OFFSET_Y;
-    var bottomCrash = snake.y + Stage.CELL > Stage.OFFSET_Y + Stage.HEIGHT;
-
-    if (leftCrash || rightCrash || topCrash || bottomCrash) {
-      return true
-    }
-
-    return false;
-  }
-  
-  function getAppleCrds() {
-    var x = Stage.OFFSET_X + (Stage.CELL * (Math.floor(Math.random() * 20)));
-    var y = Stage.OFFSET_Y + (Stage.CELL * (Math.floor(Math.random() * 15)));
-
-    var putAgain = false;
-
-    // detect apple on snake body
-    for (var i = 0; i < snake.node.length; i++) {
-      if (snake.node[i][0] == x && snake.node[i][1] == y) {
-        putAgain = true;
-      }
-    }
-
-    if (putAgain) {
-      getAppleCrds();
-    } else {
-      return [x, y];
-    }
-  }
-
-  function setTime() {
-    if (!game.over && !game.end) {
-      time._s++
-    }
-
-    if (time._s > 100) {
-      time.s++;
-      time._s = 0;
-    }
-  }
-
   /* control */
 
   function keyDownHandler(e) {
     var key = e.key; 
 
+    if (!start) {
+      start = true;
+      return;
+    }
+
     if (game.over || game.end) {
+      start = false;
       return;
     }
 
